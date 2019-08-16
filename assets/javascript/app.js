cc46d960fbb2f1bb2d3dcebfc637e91ef4793e8b
Kenya <kenya55@msn.com>
2019-08-16 13:53:35 -0400

    added unfunction for checking aswers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,3 +21,21 @@ function countdown() {
         }
     }, 1000);
 }
+function start_btn() {
+    $('.content_start').css('display', 'none');
+    $('.content_main').css('display', 'inherit');
+    countdown();
+}
+function done_btn() {
+    check_answers();
+    $('.content_main').css('display', 'none');
+    $('.content_end').css('display', 'inherit');
+}
+function check_answers() {
+    if ($('#question1_answer1').prop('checked') || $('#question1_answer2').prop('checked')) {
+        let answer1 = $('#question1_answer1').prop('checked');
+        if (answer1 === correct_answer1) corrections += 1;
+        else incorrections += 1;
+    } else {
+        unanswered += 1;
+    }
