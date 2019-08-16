e07f7fe9e9b72f0521ab184e56bf644225acbf17
Kenya <kenya55@msn.com>
2019-08-16 13:30:16 -0400

    added script links to index file, and added answers to js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,23 @@
-var secondsLeft = 30; 
-setInterval(() => {
-secondsLeft--;
-console.log(secondsLeft);
-}, 1000)
+
+var correct_answer1 = false;
+var correct_answer2 = true;
+var correct_answer3 = true;
+var correct_answer4 = false;
+var correct_answer5 = false;
+var correct_answer6 = true;
+var corrections = 0;
+var incorrections = 0;
+var unanswered = 0;
+var counts = 80;
+
+function countdown() {
+    setInterval(function () {
+        if (counts === 0) {
+            done_btn();
+            counts = 80;
+        } else {
+            counts -= 1;
+            $('#countdown').html(counts);
+        }
+    }, 1000);
+}
